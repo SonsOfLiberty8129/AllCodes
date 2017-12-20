ed7e372531f00d26b4cd201ce7a2a9f45dc6d699
SonsOfLiberty8129 <34343365+SonsOfLiberty8129@users.noreply.github.com>
2017-12-20 07:24:24 -0500

    Add files via upload

--- Teleop/BetterTeleop.java
+++ Teleop/BetterTeleop.java
@@ -19,11 +19,10 @@ public class BetterTeleop extends OpMode {
     private Servo clawServo = null;
 
 
+    public BetterTeleop() {
+    }
 
-    public BetterTeleop() {}
-
-    public void init()
-    {
+    public void init() {
         leftMotor = hardwareMap.dcMotor.get("leftDrive");
         rightMotor = hardwareMap.dcMotor.get("rightDrive");
         armMotor = hardwareMap.dcMotor.get("armMotor");
@@ -32,59 +31,48 @@ public class BetterTeleop extends OpMode {
         clawServo = hardwareMap.servo.get("gripServo");
 
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        cubeGrip.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
     }
 
-    public void loop()
-    {
+    public void loop() {
         leftMotor.setPower(gamepad1.left_stick_y);
         rightMotor.setPower(gamepad1.right_stick_y);
 
-        armMotor.setPower(gamepad2.left_stick_y);
+        Idol_Arm.setPower(gamepad2.left_stick_y);
+        armMotor.setPower(gamepad2.right_stick_y);
 
-        if(gamepad2.dpad_right)
-        {
-            cubeMotor(1, 0.5);
-
-            Stop();
-        }
-        if(gamepad2.dpad_left)
-        {
-            cubeMotor(-1  , 0.5);
-
-            Stop();
+        if (gamepad2.b) {
+            try {
+                GripTime(1, 2000);
+                cubeGrip.setPower(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-        Idol_Arm.setPower(-gamepad2.right_stick_y);
+        if (gamepad2.a) {
+            try {
+                GripTime(-1, 1200);
+                cubeGrip.setPower(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-        if(gamepad2.a)
+        if(gamepad1.x)
         {
             clawServo.setPosition(0);
         }
-        if(gamepad2.b)
+        if(gamepad1.y)
         {
             clawServo.setPosition(0.25);
         }
-
-
     }
-    public void cubeMotor (int distance, double power)
+
+    public void GripTime(double power, long time) throws InterruptedException
     {
-        cubeGrip.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        cubeGrip.setTargetPosition(distance);
-
-        cubeGrip.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
         cubeGrip.setPower(power);
+        Thread.sleep(time);
     }
-
-    public void Stop()
-    {
-        cubeGrip.setPower(0);
-    }
-
 }
 
 
